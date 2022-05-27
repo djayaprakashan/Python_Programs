361860f3c1461c04ccc46d8aecec8ba2c2afbec7
djayaprakashan <dev.jayaprakashan@gmail.com>
2022-05-27 09:36:01 -0400

    Added new program

--- Practice/Sting_Permutation2.py
+++ Practice/Sting_Permutation2.py
@@ -1,14 +1,14 @@
-userInput = input(""); # cat
-myStr = "";
-count = 0;
-myOtherStr = "";
+userInput = input("")
+myStr = ""
+count = 0
+myOtherStr = ""
 for x in userInput:
-    myStr = x;
-    nCount = 0;
-    myOtherStr = "";
+    myStr = x
+    nCount = 0
+    myOtherStr = ""
     for y in userInput:
         if nCount != count:
-            myOtherStr += y;
-        nCount+= 1;
-    print(myStr + myOtherStr);
-    print(myStr + myOtherStr[1] + myOtherStr[0]);
+            myOtherStr += y
+        nCount+= 1
+    print(myStr + myOtherStr)
+    print(myStr + myOtherStr[1] + myOtherStr[0])
